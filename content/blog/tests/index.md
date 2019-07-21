878101d0b5bd6a07e6426f0f256835f0dfbba5a7
syuni <motikix@gmail.com>
2019-07-21 09:11:45 +0900

    Fix codehighlight

---
+++ content/blog/tests/index.md
@@ -0,0 +1,15 @@
+# test
+
+## code highlight
+```js{1,7-9}
+const hoge = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+
+const fuga = (x, y, z) => {
+  return (x + y) * z;
+}
+
+// functions
+function piyo() {
+  return "piyopiyo";
+}
+```
--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,5 +2,9 @@
 import "typeface-montserrat"
 import "typeface-merriweather"
 
+// import global css
+import "./src/styles/global.css"
+
 // code highlighting
-require("prismjs/themes/prism-tomorrow.css")
+import "prismjs/themes/prism-tomorrow.css"
+import "prismjs/plugins/line-numbers/prism-line-numbers.css"
